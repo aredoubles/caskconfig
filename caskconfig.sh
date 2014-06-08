e9a9c9b75fe561cb269b6f25620886cc6ded5abf
Lim Mingjie Kenneth <kenlimmj@gmail.com>
2014-06-08 14:53:27 +0800

    Categorized all the installs

---
+++ caskconfig.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# Apps
+brew cask install dropbox
+brew cask install utorrent
+brew cask install nvalt
+brew cask install paparazzi
+brew cask install retinizer
+
+# Email
+brew cask install gpgtools
+brew cask install mailmate
+
+# Video
+brew cask install mplayer-osx-extended
+
+# Audio
+brew cask install xld
+
+# Menubar Tools
+brew cask install caffeine
+brew cask install flux
+brew cask install gfxcardstatus
+
+# Jabber Clients
+brew cask install adium
+brew cask install skype
+
+# Browsers
+brew cask install firefox
+brew cask install google-chrome
+brew cask install torbrowser
+
+# Dev Tools
+brew cask install dash
+brew cask install fontprep
+brew cask install iterm2
+brew cask install jewelrybox
+brew cask install postgres
+brew cask install sourcetree
+brew cask install sublime-text
+brew cask install xamarin-studio
+brew cask install imageoptim
+brew cask install imagealpha
+
+# Quick Look Utilities
+brew cask install betterzipql
+brew cask install qlcolorcode
+brew cask install qlmarkdown
+brew cask install qlprettypatch
+brew cask install qlstephen
+brew cask install quicklook-csv
+brew cask install quicklook-json
+brew cask install suspicious-package
+brew cask install webp-quicklook
+
+# System Utilities
+brew cask install launchrocket
+brew cask install onyx
+brew cask install the-unarchiver
+
+# System Plugins
+brew cask install flash
+brew cask install java
